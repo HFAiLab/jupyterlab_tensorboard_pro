e150485188da924ff98f300d52b5387d9e101659
aircloud <onlythen@yeah.net>
2022-07-17 21:10:58 +0800

    chore: update setup.py(language)

--- setup.py
+++ setup.py
@@ -101,6 +101,7 @@ setup_args = dict(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Jupyter",
     ],
 )
