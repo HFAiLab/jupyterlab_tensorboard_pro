ca805d3df1540614bf90deff7cc7a7e8f3d59de2
aircloud <onlythen@yeah.net>
2023-05-27 23:09:18 +0800

    chore: Temporarily use tornado==6.2 to solve compatibility issue

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup_args = dict(
     packages=setuptools.find_packages(),
     install_requires=[
         "jupyterlab",
+        "tornado==6.2",
     ],
     zip_safe=False,
     include_package_data=True,
